0c39477c512c3cd4e9c7173b76e37a9a9bc8bc53
OKUMURA Takahiro <hfm.garden@gmail.com>
2015-12-29 00:37:12 +0900

    install mogfs [wip]

--- cookbooks/mogilefs/default.rb
+++ cookbooks/mogilefs/default.rb
@@ -1,3 +1,32 @@
 git "/usr/local/mogilefs" do
   repository "https://github.com/pepabo/p5-bundle-mogilefs.git"
 end
+
+group "mogile" do
+  gid 501
+end
+
+user "mogile" do
+  uid         501
+  gid         501
+  system_user true
+  shell       "/sbin/nologin"
+end
+
+directory "/etc/mogilefs" do
+  owner "root"
+  group "root"
+end
+
+template "/etc/mogilefs/mogilefsd.conf" do
+  mode  "644"
+  owner "root"
+  group "root"
+end
+
+package "mariadb-devel"
+execute "install mogilefs" do
+  command "PATH=/opt/perl-latest/bin:$PATH sh ./install.sh -n"
+  cwd     "/usr/local/mogilefs"
+end
+
--- roles/mogfs.rb
+++ roles/mogfs.rb
@@ -1,3 +1,4 @@
 include_recipe "../cookbooks/build_essential/default.rb"
 include_recipe "../cookbooks/perl/default.rb"
-# include_recipe "../cookbooks/mogfs/default.rb"
+include_recipe "../cookbooks/mariadb/default.rb"
+include_recipe "../cookbooks/mogilefs/default.rb"
