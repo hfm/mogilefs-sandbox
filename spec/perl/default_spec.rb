fbac7c4f017f0549be4cbb63036c6232b7da998c
OKUMURA Takahiro <hfm.garden@gmail.com>
2015-12-31 16:18:25 +0900

    Add spec of symlink to perl-latest

--- spec/perl/default_spec.rb
+++ spec/perl/default_spec.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe file('/opt/perl-5.20.3/bin/perl') do
   it { should be_executable }
 end
+
+describe file('/opt/perl-latest') do
+  it { should be_linked_to '/opt/perl-5.20.3' }
+end
